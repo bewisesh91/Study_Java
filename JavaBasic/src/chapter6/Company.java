a0374481610e7950e06583b6cfcf1008a10868f2
bewisesh91 <bewise.seunghyun@gmail.com>
2021-07-02 00:46:52 +0900

    updata java basic

---
+++ JavaBasic/src/chapter6/Company.java
@@ -0,0 +1,12 @@
+package chapter6;
+
+public class Company {
+    private Company() {}
+    private static Company instance = new Company();
+    public static Company getInstance() {
+        if (instance == null) {
+            instance = new Company();
+        }
+        return instance;
+    }
+}
